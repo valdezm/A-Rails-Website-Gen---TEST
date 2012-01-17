90e2641e805233605b95c8e4c6564cef4cfaad7f
mark valdez <valdezm@uw.edu>
2012-01-16 17:07:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valdez::Application.config.secret_token = '9836b5fa94d75373867c6477d2be3b519629ee1cf1c9c280e3aea8aa1d3cda5c971924e41dd41e95b2289d679686d0530831cac08abd1183a0104cac59222a14'
